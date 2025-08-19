5460e99fdfbea3174c764a0c0a3043ff966d7671
Ronald <ronaldkiptoo64@gmail.com>
2025-08-19 03:02:47 +0300

    Task 1: Added recipe details page with editing and deleting functionality

--- recipe-sharing-app/src/store/recipStore.js
+++ recipe-sharing-app/src/store/recipStore.js
@@ -4,5 +4,14 @@ export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
+        r.id === id ? { ...r, ...updatedRecipe } : r
+      ),
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
+    })),
 }));
